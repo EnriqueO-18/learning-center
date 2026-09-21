88121ae5cf890ea107e2934ce1dc2459a2799471
Enrique <ochoaenriqu@gmail.com>
2026-09-21 17:54:21 -0500

    feat: add shared

---
+++ src/shared/presentation/components/Views/about.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
+</script>
+
+<template>
+  <section class="mt-6 p-4 md:p-5">
+    <div class="max-w-lg mx-auto gap-3">
+      <h1 class="text-4xl font-bold text-color">{{ t('about.title') }}</h1>
+      <p class="text-lg font-light text-color-secondary">{{ t('about.content') }}</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
